96b32f24bbb74fde3ff67b576ed9ccf6fd9c4664
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-27 21:21:15 -0300

    update

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("hellos");
+var a;
+a = 3;
+console.log(a);
+
+var then = new Date();
+console.log(then);
+
+// Trabalhando com strings
+
+const msg = "hello " + "world";
+// console.log(msg);
+console.log(msg.length);
+
+// Valores booleanos
